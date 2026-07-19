412dd73a8f99095babe5064221230c47d1375e39
jexoinfo-cmd <jexo.info@gmail.com>
2026-07-19 20:40:42 +0600

    Fix middleware blocking public assets

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,7 @@ export function middleware(request: NextRequest) {
 
   if (
     pathname.startsWith('/_next') ||
+    pathname.startsWith('/assets') ||
     pathname === '/favicon.ico'
   ) {
     return NextResponse.next()
